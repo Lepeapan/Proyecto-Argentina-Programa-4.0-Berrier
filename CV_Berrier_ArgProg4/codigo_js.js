eae3e70fbf33d9eb024b50e12ae66356a68d32eb
leandro berrier <berrierleandro@gmail.com>
2023-01-27 12:09:21 -0300

    intentando guardar objeto con datos de api

--- CV_Berrier_ArgProg4/codigo_js.js
+++ CV_Berrier_ArgProg4/codigo_js.js
@@ -1,11 +1,27 @@
 /** código utilizado para darle animacion y funcionalidad al Proyecto */
 
 /** de la página sugerida para obtener datos aleatorios se obtiene información del usuario */
-
+let usuario
 fetch('https://randomuser.me/api/1.4/?gender=male')
   .then((response) => response.json())
   .then(data => {usuario = data.results[0];})
-  .then(() => {console.log(usuario);});
+  //.then(() => {console.log(usuario);});  
+console.log(usuario);
 
+/** -------------------codigo de MEDIUM.com---------------------- */
+
+// const apiData = () => {
+//     fetch('https://randomuser.me/api/1.4/?gender=male')
+//       .then((response) => {
+//         return response.json()
+//     }).then((data) => {
+//         fetchedData(data)
+//     })
+// }
+
+// fetchedData = (apiData) => {
+//   console.log(apiData)
+// }
+//console.log(apiData)
 
 
