54fbec9a21186cec928712bb2c4ad684df27da5c
leandro berrier <berrierleandro@gmail.com>
2023-02-03 13:30:09 -0300

    trabajando en el fetch como funcion

--- CV_Berrier_ArgProg4/codigo_js.js
+++ CV_Berrier_ArgProg4/codigo_js.js
@@ -1,11 +1,22 @@
 /** código utilizado para darle animacion y funcionalidad al Proyecto */
 
 /** de la página sugerida para obtener datos aleatorios se obtiene información del usuario */
-let usuario
-fetch('https://randomuser.me/api/1.4/?gender=male')
-  .then((response) => response.json())
-  .then(data => {usuario = data.results[0];})
-  .then(() => {console.log(usuario);});  
+/** Esto está andando */
+// let usuario
+// fetch('https://randomuser.me/api/1.4/?gender=male')
+//   .then((response) => response.json())
+//   .then(data => {usuario = data.results[0];})
+//   .then(() => {console.log(usuario);});  
+
+
+//*-------------------------------------------intentando que ande como función----------------------------------------//
+  let usuario
+  function ObtenerDatos(usuario) {
+    fetch('https://randomuser.me/api/1.4/?gender=male')
+      .then((response) => response.json())
+      .then(data => {usuario = data.results[0];})
+      .then(() => {console.log(usuario);});
+  }
 //console.log(usuario);
 
 /** -------------------codigo de MEDIUM.com---------------------- */
